2c73f9ca3ca122597a3c0683151daa7c53460da2
Feng Wang <wang6922@outlook.com>
2025-09-20 12:27:10 +0800

    Add password reset function (#19)
    Provide tool for the server owner to reset passwords for any user.

--- src/tui-register.cpp
+++ src/tui-register.cpp
@@ -4,6 +4,8 @@
 #include <stdexcept>
 #include <string>
 #include <optional>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include <tev-cpp/Tev.h>
 #include <js-style-co-routine/Promise.h>
@@ -71,6 +73,7 @@ struct AppParams
 
 static JS::Promise<void> MainNoexceptAsync(Tev& tev, AppParams params);
 static JS::Promise<void> MainAsync(Tev& tev, AppParams params);
+static JS::Promise<std::string> ReadLineAsync(Tev& tev);
 
 int main(int argc, char const *argv[])
 {
@@ -113,12 +116,6 @@ static JS::Promise<void> MainNoexceptAsync(Tev& tev, AppParams params)
 
 static JS::Promise<void> MainAsync(Tev& tev, AppParams params)
 {
-    auto database = co_await Database::Database::CreateAsync(tev, params.dbPath.value());
-    /** Check if there are any existing user */
-    if (!database->ListUser().empty())
-    {
-        throw std::runtime_error("There are existing users. You can only register the first user with this program.");
-    }
     /** Parser the registration string to user info and credential */
     auto bytes = Common::Base64::Decode(params.registerString.value());
     auto tlv = Common::Tlv<Application::RegisterTlvType>::Parse(bytes);
@@ -180,17 +177,124 @@ static JS::Promise<void> MainAsync(Tev& tev, AppParams params)
     credential.set_salt(Common::Base64::Encode(saltOpt.value()));
     credential.set_w0(Common::Base64::Encode(w0Opt.value()));
     credential.set_l(Common::Base64::Encode(LOpt.value()));
-    Schema::IServer::UserAdminSettings adminSettings;
-    using RoleType = std::remove_reference_t<decltype(adminSettings.get_mutable_role())>;
-    /** The first user will always be admin */
-    adminSettings.set_role(RoleType::ADMIN);
-    auto userId = co_await database->CreateUserAsync(
-        username,
-        static_cast<nlohmann::json>(adminSettings).dump(),
-        static_cast<nlohmann::json>(credential).dump());
-    if (!publicMetadata.empty())
+    
+    auto database = co_await Database::Database::CreateAsync(tev, params.dbPath.value());
+    /**
+     * Check if:
+     * 1. There is an existing user. If so, this is a password reset.
+     * 2. There is an existing admin user. If not, this is the first admin registration. This can happen if:
+     *     a. The server was just created and there is no user.
+     *     b. The last admin deleted themselves.
+     * If neither is true, reject the registration.
+     */
+    auto userList = database->ListUser();
+    std::optional<Common::Uuid> existingUserId{ std::nullopt };
+    bool hasAdmin = false;
+    for (const auto& user : userList)
     {
-        co_await database->SetUserPublicMetadataAsync(userId, publicMetadata);
+        if (user.userName == username)
+        {
+            existingUserId = user.id;
+        }
+        auto adminSettingsObj = nlohmann::json::parse(user.adminSettings);
+        Schema::IServer::UserAdminSettings adminSettings = adminSettingsObj.get<Schema::IServer::UserAdminSettings>();
+        using RoleType = std::remove_reference_t<decltype(adminSettings.get_mutable_role())>;
+        if (adminSettings.get_role() == RoleType::ADMIN)
+        {
+            hasAdmin = true;
+        }
     }
-    std::cout << "First admin registered successfully: " << username << std::endl;
+    
+    if (existingUserId.has_value())
+    {
+        std::cout << "User " << username << " already exists. This will reset the password." << std::endl;
+        std::cout << "Are you sure to continue? (Y/[N]): " << std::flush;
+        auto answer = co_await ReadLineAsync(tev);
+        if (answer != "Y" && answer != "y")
+        {
+            throw std::runtime_error("Aborted by user");
+        }
+        co_await database->SetUserCredentialAsync(existingUserId.value(), static_cast<nlohmann::json>(credential).dump());
+        if (!publicMetadata.empty())
+        {
+            co_await database->SetUserPublicMetadataAsync(existingUserId.value(), publicMetadata);
+        }
+        std::cout << "Password reset successfully for user: " << username << std::endl;
+    }
+    else if (!hasAdmin)
+    {
+        std::cout << "No admin user found. Registering the first admin user: " << username << std::endl;
+        std::cout << "Are you sure to continue? (Y/[N]): " << std::flush;
+        auto answer = co_await ReadLineAsync(tev);
+        if (answer != "Y" && answer != "y")
+        {
+            throw std::runtime_error("Aborted by user");
+        }
+        /** The first user will always be admin */
+        Schema::IServer::UserAdminSettings adminSettings;
+        using RoleType = std::remove_reference_t<decltype(adminSettings.get_mutable_role())>;
+        adminSettings.set_role(RoleType::ADMIN);
+        auto userId = co_await database->CreateUserAsync(
+            username,
+            static_cast<nlohmann::json>(adminSettings).dump(),
+            static_cast<nlohmann::json>(credential).dump());
+        if (!publicMetadata.empty())
+        {
+            co_await database->SetUserPublicMetadataAsync(userId, publicMetadata);
+        }
+        std::cout << "First admin registered successfully: " << username << std::endl;
+    }
+    else
+    {
+        throw std::runtime_error("This is neither a password reset nor the first admin registration");
+    }
+}
+
+static void SetToUnblocking(int fd)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1)
+    {
+        throw std::runtime_error("Failed to get file descriptor flags");
+    }
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1)
+    {
+        throw std::runtime_error("Failed to set file descriptor to non-blocking");
+    }
+}
+
+static JS::Promise<std::string> ReadLineAsync(Tev& tev)
+{
+    SetToUnblocking(STDIN_FILENO);
+    std::string buffer;
+    JS::Promise<void> promise;
+    auto readHandler = tev.SetReadHandler(STDIN_FILENO, [&buffer, promise](){
+        char c;
+        ssize_t n = read(STDIN_FILENO, &c, 1);
+        if (n == -1)
+        {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+            {
+                return;
+            }
+            promise.Reject("Failed to read from stdin");
+        }
+        else if (n == 0)
+        {
+            promise.Reject("EOF reached");
+        }
+        else
+        {
+            if (c == '\n')
+            {
+                promise.Resolve();
+            }
+            else
+            {
+                buffer.push_back(c);
+            }
+        }
+    });
+    co_await promise;
+    co_return buffer;
 }
